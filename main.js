b38c9dc0b7cb96737292dd46c660e2d15cd463b5
Alessandro10Lima <95730567+Alessandro10Lima@users.noreply.github.com>
2022-12-20 15:06:54 -0300

    atualização do codigo

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 const form = document.getElementById('form-number');
+const segundoNumero = document.getElementById("numero-dois");
 const primeiroNumero = document.getElementById('numero-um');
 let formValido = false;
 
@@ -8,11 +9,9 @@ function validaNumero(numero) {
 }
 form.addEventListener('submit', function(e) {
     e.preventDefault();
-    
-    const segundoNumero = document.getElementById("numero-dois");
     const mensagemSucesso = `Parabéns! O segundo número: <b>${segundoNumero.value}</b> é maior que: <b>${primeiroNumero.value}</b>`;
-    formValido = validaNumero(segundoNumero.value)
-
+    
+    formValido = validaNumero(segundoNumero.value);
     if (formValido) {
         const containerMensagemSucesso = document.querySelector(".sucess-msn");
         containerMensagemSucesso.innerHTML = mensagemSucesso;
@@ -25,16 +24,15 @@ form.addEventListener('submit', function(e) {
         document.querySelector('.error-msn').style.display = 'block';
     }
 })
-segundoNumero.addEventListener('keyup', function(e){
-console.log(e.target.value);
-formValido = validaNumero(e.target.value);
+segundoNumero.addEventListener('keyup', function(e) {
+    console.log(e.target.value);
+    formValido = validaNumero(e.target.value);
 
-if (!formValido) {
-    segundoNumero.classList.add('error');
-    // segundoNumero.style.border = '1px solid red';
-    document.querySelector('.error-msn').style.display = 'block';
-} else {
-    segundoNumero.classList.remove('error');
-    document.querySelector('.error-msn').style.display = 'none';
-}
+    if(!formValido){
+        segundoNumero.classList.add('error');
+        document.querySelector('.error-msn').style.display = 'block';
+    } else{
+        segundoNumero.classList.remove('error');
+        document.querySelector('error-msn').style.display = 'none';
+    }
 })
